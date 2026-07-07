264f0d78f9dfc5164657a29f275f9d86a5d02e21
Puneeth R <puneethraj107@gmail.com>
2026-07-07 15:10:13 +0530

    started with javascript

--- Dy4/hello.js
+++ Dy4/hello.js
@@ -4,8 +4,8 @@ var b = "8";
 /***********Do not change the code above 👆*******/
     //Write your code on lines 7 - 9:
 var c = a;
-var a = b;
-var b = c;
+ a = b;
+ b = c;
 
 /***********Do not change the code below 👇*******/
 
