4b0eaa3c0e573868b4a55500e03a229ffb3ddf44
Puneeth R <puneethraj107@gmail.com>
2026-07-07 15:09:00 +0530

    started with javascript

--- Dy4/hello.js
+++ Dy4/hello.js
@@ -1,1 +1,14 @@
-console.log("it's finally time for javascript");
+var a = "3";
+var b = "8";
+
+/***********Do not change the code above 👆*******/
+    //Write your code on lines 7 - 9:
+var c = a;
+var a = b;
+var b = c;
+
+/***********Do not change the code below 👇*******/
+
+console.log("a is " + a);
+console.log("b is " + b);
+
