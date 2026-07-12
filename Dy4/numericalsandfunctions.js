4474c9d3c6e0370a99f9aa1742df32aa9ce62802
Puneeth R <puneethraj107@gmail.com>
2026-07-12 17:16:02 +0530

    learnt about function and various mathematical operators

---
+++ Dy4/numericalsandfunctions.js
@@ -0,0 +1,34 @@
+//
+var age_of_dog = prompt("enter the dogs age: ");
+var human_eq_age = (age_of_dog - 2) * 4 + 21;
+alert("age of your dog in human equivalent terms is "+human_eq_age);
+
+// X++ is increment operator , X-- is decrement operator
+// X+=2
+
+// function name() {
+// }
+function getMilk(money) {
+    var bottles = parseInt(money / 1.5) ;
+    console.log("leaveHouse");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveUp");
+    console.log("moveRight");
+    console.log("moveRight");
+    console.log("bought "+ bottles + " bottles of milk ") // can also use math.floor(_);
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveDown");
+    console.log("moveLeft");
+    console.log("moveLeft");
+    console.log("enterHouse");
+}
+
+getMilk(5);
