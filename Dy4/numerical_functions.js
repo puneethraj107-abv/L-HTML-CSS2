2b7e0a89a2414e5bbef1e05b8ea9b63bd02b37e2
Puneeth R <puneethraj107@gmail.com>
2026-07-12 17:05:33 +0530

    learnt about function and various mathematical operators

--- Dy4/numerical_functions.js
+++ Dy4/numerical_functions.js
@@ -1,7 +1,7 @@
 //
-// var age_of_dog = prompt("enter the dogs age: ");
-// var human_eq_age = (age_of_dog - 2) * 4 + 21;
-// alert("age of your dog in human equivalent terms is "+human_eq_age);
+var age_of_dog = prompt("enter the dogs age: ");
+var human_eq_age = (age_of_dog - 2) * 4 + 21;
+alert("age of your dog in human equivalent terms is "+human_eq_age);
 
 // X++ is increment operator , X-- is decrement operator
 // X+=2
