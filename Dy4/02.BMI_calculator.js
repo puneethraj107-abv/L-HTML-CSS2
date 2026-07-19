2f87338521cd42448f73910502b0657ff05e545b
Puneeth R <puneethraj107@gmail.com>
2026-07-19 15:47:38 +0530

    learnt about function with returning values and nesting functions. 19-07 learnt about if else statment.

--- Dy4/02.BMI_calculator.js
+++ Dy4/02.BMI_calculator.js
@@ -1,6 +1,12 @@
 function BMIcalculator(weight,height){
     var BMI = Math.ceil(weight/(height*height));
-    console.log("your BMI is "+BMI);
+    if (BMI<18.5) {
+        console.log("Your BMI is "+BMI+", so you are underweight.");
+    } if(BMI>=18.5 && BMI <= 24.9) {
+        console.log("Your BMI is "+BMI+", so you have normal weight.");
+    } if (BMI >24.9) {
+        console.log("Your BMI is "+BMI+", so you are overweight.");
+    }
 }
 
 BMIcalculator(54,1.73);
