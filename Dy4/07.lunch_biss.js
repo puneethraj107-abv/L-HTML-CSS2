0d498394ec07fb977813ff2cc352b6e8b775da7f
Puneeth R <puneethraj107@gmail.com>
2026-07-25 15:10:56 +0530

    just a simple challenge.

---
+++ Dy4/07.lunch_biss.js
@@ -0,0 +1,17 @@
+var friends=["puneeth","fisal","tejas","abhi"];
+
+function getRandomElement(array) {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+function whosPaying(names) {
+
+    /******Don't change the code above*******/
+
+    //Write your code here.
+    console.log(getRandomElement(names)+" is going to pay the bill");
+
+    /******Don't change the code below*******/
+}
+
+whosPaying(friends);
