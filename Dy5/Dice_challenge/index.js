8bde1599a78618ff4053a3068d300d0038aa2c13
Puneeth R <puneethraj107@gmail.com>
2026-08-15 00:02:15 +0530

    learnt to integrate javascript into html & using DOM to manipulate html elements and create a dice game

---
+++ Dy5/Dice_challenge/index.js
@@ -0,0 +1,21 @@
+let random_number1=Math.floor(Math.random()*6)+1;
+
+let randomdiceimg="images/dice"+random_number1+".png";
+
+let image1=document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomdiceimg);
+
+let random_number2=Math.floor(Math.random()*6)+1;
+
+let randomdiceimg2="images/dice"+random_number2+".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomdiceimg2);
+
+if (random_number1>random_number2) {
+    document.querySelector("h1").innerHTML="Player 1 wins sukker!";
+}
+else if(random_number2>random_number1) {
+    document.querySelector("h1").innerHTML="Player 2 wins asshole!";
+} else {
+    document.querySelector("h1").innerHTML="its a Draw";
+}
