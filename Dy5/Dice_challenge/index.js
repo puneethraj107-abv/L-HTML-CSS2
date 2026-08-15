5e3e520d0b84a41b71abb3fccf2bbaec3e8065fb
Puneeth R <puneethraj107@gmail.com>
2026-08-15 15:24:06 +0530

    learned to link javascript with HTML&CSS, and manipulate HTML elements in browser console using javascript.

--- Dy5/Dice_challenge/index.js
+++ Dy5/Dice_challenge/index.js
@@ -1,3 +1,4 @@
+//creating 1st random number
 let random_number1=Math.floor(Math.random()*6)+1;
 
 let randomdiceimg="images/dice"+random_number1+".png";
@@ -5,12 +6,14 @@ let randomdiceimg="images/dice"+random_number1+".png";
 let image1=document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomdiceimg);
 
+//creating 2nd random number
 let random_number2=Math.floor(Math.random()*6)+1;
 
 let randomdiceimg2="images/dice"+random_number2+".png";
 
 document.querySelectorAll("img")[1].setAttribute("src", randomdiceimg2);
 
+//the logic to determine winner
 if (random_number1>random_number2) {
     document.querySelector("h1").innerHTML="Player 1 wins sukker!";
 }
