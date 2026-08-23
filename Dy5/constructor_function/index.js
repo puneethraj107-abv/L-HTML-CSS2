3ebe0af22e10f51bae6279df97c47996d956c264
Puneeth R <puneethraj107@gmail.com>
2026-08-23 19:45:45 +0530

    constructor function

---
+++ Dy5/constructor_function/index.js
@@ -0,0 +1,12 @@
+function BellBoy(name,age,work_permit,language){
+    this.namee=name;
+    this.age=age;
+    this.work_permit=work_permit;
+    this.language=language;
+    this.cleann=function () {
+        console.log("cleaning")
+
+    }
+}
+
+let bellboy1=new BellBoy('dhanush',21,true,['kannada','hindi','english']);
