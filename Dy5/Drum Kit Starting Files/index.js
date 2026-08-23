57da652601a61b642ed9d3452230631049919938
Puneeth R <puneethraj107@gmail.com>
2026-08-23 21:17:47 +0530

    constructor function,fixed the for loop,timeout function

--- Dy5/Drum Kit Starting Files/index.js
+++ Dy5/Drum Kit Starting Files/index.js
@@ -1,51 +1,67 @@
-let numberofbuttons=document.querySelectorAll(".drum").length;//".____" use it to select class
+let numberofbuttons = document.querySelectorAll(".drum").length;
 
-for (let i=0;i<=numberofbuttons;i++) {
-    document.querySelectorAll("button")[i].addEventListener("click", handleClick);//don't add () to the function in event listeners
-
+for (let i = 0; i < numberofbuttons; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 }
 
-function handleClick() {//can use anonymous function but prefer named func.
-    let buttonhtml=this.innerHTML;
-    switch (buttonhtml){
+function handleClick() {
+    let buttonhtml = this.innerHTML;
+    makeSound(buttonhtml);
+    buttonAnimation(buttonhtml);
+}
+
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makeSound(key){
+    switch (key) {
         case 'w':
-        let tom1=new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
 
         case 'a':
-            let tom2=new Audio("sounds/tom-2.mp3");
+            let tom2 = new Audio("sounds/tom-2.mp3");
             tom2.play();
             break;
 
         case 's':
-            let tom3=new Audio("sounds/tom-3.mp3");
+            let tom3 = new Audio("sounds/tom-3.mp3");
             tom3.play();
             break;
+
         case 'd':
-            let tom4=new Audio("sounds/tom-4.mp3");
+            let tom4 = new Audio("sounds/tom-4.mp3");
             tom4.play();
             break;
+
         case 'j':
-            let snare=new Audio("sounds/snare.mp3");
+            let snare = new Audio("sounds/snare.mp3");
             snare.play();
             break;
+
         case 'k':
-            let crash=new Audio("sounds/crash.mp3");
+            let crash = new Audio("sounds/crash.mp3");
             crash.play();
             break;
+
         case 'l':
-            let kick_bass=new Audio("sounds/kick-bass.mp3");
+            let kick_bass = new Audio("sounds/kick-bass.mp3");
             kick_bass.play();
             break;
-
-        default:
-            console.log(buttonhtml);
-
     }
+
 }
 
-
-
+function buttonAnimation(currentkey){
+    let activebutton=document.querySelector("."+currentkey);
+    activebutton.classList.add("pressed");
+    setTimeout(function (){
+        activebutton.classList.remove("pressed")
+    },100)
+}
 
 //In JavaScript, this is a special keyword that refers to the object/context that is currently calling the function.
