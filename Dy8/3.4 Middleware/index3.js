c7808466f6ba963eeadbf02b20578dc4422d44e1
Puneeth R <puneethraj107@gmail.com>
2026-09-26 01:10:59 +0530

    middlewares and working in multiple pages

--- Dy8/3.4 Middleware/index3.js
+++ Dy8/3.4 Middleware/index3.js
@@ -3,6 +3,13 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// custom function here
+function logger(req, res, next) {
+  console.log("Request Method: ", req.method);
+  console.log("Request URL: ", req.url);
+  next()//system hangs if next() is not used
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
